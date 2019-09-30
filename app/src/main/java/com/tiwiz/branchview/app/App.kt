c1049fdaa617700ce63380525ac08cfde4474d8a
Roberto Orgiu <roberto.orgiu@gmail.com>
2019-09-30 11:37:13 +0200

    Add dependency

--- app/src/main/java/com/tiwiz/branchview/app/App.kt
+++ app/src/main/java/com/tiwiz/branchview/app/App.kt
@@ -1,10 +1,13 @@
 package com.tiwiz.branchview.app
 
 import android.app.Application
+import com.thesaurus.branchview.BranchIndicator
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        BranchIndicator.attachTo(this, "BranchView")
     }
 }
